0dea6a8e8e761378280059169632f30f62219953
U-kom2\Tomek <Tomasz.Put@gmail.com>
2013-04-21 13:14:43 +0200

    Removing deleted files from repo

--- src/server/Node.java
+++
@@ -1,8 +0,0 @@
-package server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface Node extends Remote {
-    String addAgent(Integer energy) throws RemoteException;
-}
